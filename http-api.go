9887586ffea8773977cf4238650abe411fa07bfe
Dhananjay Balan <dhananjay@plivo.com>
2015-02-14 20:11:43 +0530

    Use the interface to serve http

--- http-api.go
+++ http-api.go
@@ -6,10 +6,20 @@ import (
 	"net/http"
 )
 
+type Person struct {
+	name string
+}
+
+func (p Person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Dude's called : %s", p.name)
+}
+
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world")
 	})
+	http.Handle("/foo", Person{"Bob"})
 
 	http.ListenAndServe(":8000", nil)
 }
