7ce1560cf6c5063757ac8e324a6cfc7d1e90aae7
Dhananjay Balan <dhananjay@plivo.com>
2015-02-20 12:00:27 +0530

    Stream file back using http

--- http/http-client.go
+++ http/http-client.go
@@ -5,7 +5,9 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -16,6 +18,24 @@ func main() {
 	client := &http.Client{
 		Transport: tr,
 		Timeout:   time.Duration(5 * time.Second)}
-	resp, _ := client.Get("https://server.with.invalid.cert")
-	fmt.Println(resp)
+	resp, _ := client.Get("http://path.to.wav")
+	fmt.Println(resp.Header["Cache-Control"][0])
+
+	for k, v := range strings.Split(resp.Header["Cache-Control"][0], ",") {
+		fmt.Println(k, v)
+	}
+	defer resp.Body.Close()
+
+	data, _ := ioutil.ReadAll(resp.Body)
+	clength := fmt.Sprintf("%d", len(data))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "audio/wav")
+		w.Header().Set("Content-Length", clength)
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("User-Agent", "PlivoGoTTS/0.1")
+		w.Write(data)
+	})
+	http.ListenAndServe(":8000", nil)
+
 }
