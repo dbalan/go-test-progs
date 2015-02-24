2dbf3de1907d678986a8f957ca306a98163bae43
Dhananjay Balan <dhananjay@plivo.com>
2015-02-24 16:20:34 +0530

    LTS server with self-signed cert

---
+++ https/https-server.go
@@ -0,0 +1,27 @@
+// Just return some stuff to get
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Person struct {
+	name string
+}
+
+func (p Person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Dude's called : %s", p.name)
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("cache-control", "public")
+		fmt.Printf("Method: %s\nURL: %s\nHeader: %s\nReqURI: %s \n", r.Method, r.URL, r.Header, r.RequestURI)
+
+	})
+	http.Handle("/foo", Person{"Bob"})
+
+	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+}
